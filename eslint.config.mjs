2168e15bdd21ed74aa8a1320f54650b4a3577785
harsimranjit1 <harsimranjitkaurkhinda@gmail.com>
2025-07-31 01:32:39 -0400

    Update npm config and add ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+];
