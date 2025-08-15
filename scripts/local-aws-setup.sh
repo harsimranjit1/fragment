525eb2de7c2bafd3961717a57f14b36870d8572e
harsimranjit1 <harsimranjitkaurkhinda@gmail.com>
2025-08-14 22:23:58 -0400

    Lab 10: add local-aws-setup.sh

--- scripts/local-aws-setup.sh
+++ scripts/local-aws-setup.sh
@@ -21,7 +21,7 @@ echo "Creating LocalStack S3 bucket: $BUCKET_NAME"
 aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket "$BUCKET_NAME" || true
 
 # Create DynamoDB table
-TABLE_NAME="${AWS_DYNAMODB_TABLE_NAME:-harsimranjit-fragment}"
+TABLE_NAME="${AWS_DYNAMODB_TABLE_NAME:-fragments}"
 echo "Creating DynamoDB-Local table: $TABLE_NAME"
 aws --endpoint-url=http://localhost:8000 \
   dynamodb create-table \
